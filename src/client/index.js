59468bce3949ce4e75b5f9d410160e2e5e5ddad3
hayato423 <hayato0423terao@gmail.com>
2020-08-12 17:24:53 +0900

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import axios from 'axios';
+
+export const Index = () => {
+    const [data, setData] = useState('テスト');
+
+    useEffect(()=> {
+        const fetchData = async () => {
+            try {
+                const result = await axios.get('http://127.0.0.1:3000/api');
+                console.log(result);
+                setData(result.data.api2);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
+    })
+
+
+
+    return (
+        <div>
+            <h1>React!!</h1>
+            <h1>{data}</h1>
+            <a href='/twitter/auth'>twitter</a>
+        </div>
+    );
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
---
+++ src/server/server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const TwitterStrategy = require('passport-twitter').Strategy;
+
+require('dotenv').config();
+
+
+const app = express();
+
+//セッション初期化
+app.use(session({
+    secret : 'secret-key',
+    resave : true,
+    saveUninitialized : false,
+    //cookie : {secure:true}
+  }));
+
+//passport初期化
+app.use(passport.initialize());
+app.use(passport.session());
+
+//passport初期化
+app.use(passport.initialize());
+app.use(passport.session());
+
+//セッションのシリアライズ化
+passport.serializeUser(function(user,done) {
+  console.log("Serialize..")
+  done(null,user);
+});
+
+//セッションのデシリアライズ化
+passport.deserializeUser(function(obj, done) {
+  console.log("DeSerialize..")
+  done(null,obj);
+});
+
+passport.use(new TwitterStrategy({
+  consumerKey : process.env.TWITTER_API_KEY,
+  consumerSecret : process.env.TWITTER_API_KEY_SECRET,
+  callbackURL : 'http://127.0.0.1:3000/twitter/callback'
+},function(token,tokenSecret, profile, callback) {
+  console.log("認証しました")
+  return callback(null,profile);
+}));
+
+//distディレクトリ内の静的ファイルにアクセス
+app.use(express.static(path.resolve('./','dist')));
+
+app.get('/api',(req,res) => {
+    res.send({
+        api: 'test',
+        api2: 'TEST'
+    });
+})
+
+app.get('/twitter/auth',passport.authenticate('twitter'));
+app.get('/twitter/callback',passport.authenticate('twitter',{
+  failureRedirect: '/'
+}),function(req,res){
+  res.redirect('/home');
+});
+
+app.get('*',function(req,res) {
+    res.sendFile(path.resolve('./','dist','index.html'));
+    console.log(req.session);
+})
+
+app.listen(3000, () => {
+    console.log('server running');
+})
