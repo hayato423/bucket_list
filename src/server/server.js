9dbf813985862f085e64b4f24de999c41249bb86
hayato423 <hayato0423terao@gmail.com>
2020-08-15 02:27:45 +0900

    バケツリスト一覧取得api作成

--- src/server/server.js
+++ src/server/server.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 const { ESRCH } = require('constants');
 const TwitterStrategy = require('passport-twitter').Strategy;
 const mysql = require('mysql');
+const crypto = require('crypto');
 
 
 require('dotenv').config();
@@ -13,21 +14,21 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //セッション初期化
 app.use(session({
-    secret : 'secret-key',
-    resave : true,
-    saveUninitialized : false,
-    cookie : {
-      secure: false,
-    httpOnly : true,
-    maxAge : 24 * 60 * 60 * 1000
-      }
-  }));
+  secret: 'secret-key',
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
 
 //passport初期化
 app.use(passport.initialize());
@@ -38,73 +39,110 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //セッションのシリアライズ化
-passport.serializeUser(function(user,done) {
+passport.serializeUser(function (user, done) {
   //console.log("Serialize..")
-  done(null,user);
+  done(null, user);
 });
 
 //セッションのデシリアライズ化
-passport.deserializeUser(function(obj, done) {
+passport.deserializeUser(function (obj, done) {
   //console.log("DeSerialize..")
-  done(null,obj);
+  done(null, obj);
 });
 
 passport.use(new TwitterStrategy({
-  consumerKey : process.env.TWITTER_API_KEY,
-  consumerSecret : process.env.TWITTER_API_KEY_SECRET,
-  callbackURL : 'http://127.0.0.1:3000/twitter/callback'
-},function(token,tokenSecret, profile, callback) {
+  consumerKey: process.env.TWITTER_API_KEY,
+  consumerSecret: process.env.TWITTER_API_KEY_SECRET,
+  callbackURL: 'http://127.0.0.1:3000/twitter/callback'
+}, function (token, tokenSecret, profile, callback) {
   //console.log("認証しました")
-  return callback(null,profile);
+  return callback(null, profile);
 }));
 
 //distディレクトリ内の静的ファイルにアクセス
-app.use(express.static(path.resolve('./','dist')));
+app.use(express.static(path.resolve('./', 'dist')));
+
 
 const con = mysql.createConnection({
   host: 'localhost',
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD
+  password: process.env.MYSQL_PASSWORD,
+  database: 'bucketList'
 });
 
 
-app.get('/api/user',(req,res) => {
-  if(req.session.passport != undefined){
+app.get('/api/user', (req, res) => {
+  if (req.session.passport != undefined) {
     res.send(req.session.passport.user);
-  }else{
+  } else {
     res.status(404).send(null);
   }
 })
 
-app.post('/api/createlist',(req,res) => {
-  console.log(req.body);
+app.post('/api/createlist', (req, res) => {
+  const createRandomID = () => {
+    const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let T = '';
+    const N = 10;
+    for (var i = 0; i < N; i++) {
+      const n = Math.floor(Math.random() * S.length);
+      T = T + S[n];
+    }
+    return T;
+  }
+
+  let list_id = createRandomID();
   const twitter_id = req.session.passport.user.id;
-  const twitter_name = req.session.passport.user.displayName;
   const list_title = req.body.title;
   const items = req.body.items;
   const json = {
     twitter_id : twitter_id,
-    twitter_name: twitter_name,
+    list_id : list_id,
     list_title : list_title,
-    items: items
+    items : items
   }
-  res.status(200).send(json);
+  var status = 200;
+  try {
+    items.forEach((item, index) => {
+      con.query("insert into bucketlist values(?,?,?,?,?,?)",
+        [twitter_id, list_id, list_title, index + 1, item, 0]);
+    });
+  } catch (error) {
+    console.log(error);
+    status = 500;
+  }finally{
+    res.status(status).send(json);
+  }
+
 });
 
 
-app.get('/twitter/auth',passport.authenticate('twitter'));
-app.get('/twitter/callback',passport.authenticate('twitter',{
+
+app.get('/api/listcatalog', (req,res) => {
+  const twitter_id = req.session.passport.user.id;
+  con.query('select distinct list_id, list_title  from bucketlist where twitter_id=?',[twitter_id],(error,result,fields)=> {
+    console.log(result);
+    res.send(result);
+  })
+});
+
+
+app.get('/twitter/auth', passport.authenticate('twitter'));
+app.get('/twitter/callback', passport.authenticate('twitter', {
   failureRedirect: '/'
-}),function(req,res){
+}), function (req, res) {
   //console.log(req.session);
   res.redirect('/home');
 });
 
-app.get('*',function(req,res) {
-    res.sendFile(path.resolve('./','dist','index.html'));
-    //console.log(req.session);
+
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve('./', 'dist', 'index.html'));
+  //console.log(req.session);
 })
 
+
 app.listen(3000, () => {
-    console.log('server running');
+  console.log('server running');
 })
+
