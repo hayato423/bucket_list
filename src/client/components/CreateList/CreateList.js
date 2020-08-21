ddf931af6266ee556372943b1271104a78669fc4
hayato423 <hayato0423terao@gmail.com>
2020-08-21 22:41:11 +0900

    フロント側の入力文字数チェック追加

--- src/client/components/CreateList/CreateList.js
+++ src/client/components/CreateList/CreateList.js
@@ -1,41 +1,49 @@
-import React ,{useState,useEffect}from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "./style.css";
 
 
 export const CreateList = () => {
-  const [title,setTitle] = useState('');
-  const [itemList,setItemList] = useState([]);
-  const [item,setItem] = useState('');
+  const [title, setTitle] = useState('');
+  const [itemList, setItemList] = useState([]);
+  const [item, setItem] = useState('');
 
   const addItem = () => {
-    if(item != ''){
-      setItemList([...itemList,item]);
-      setItem('');
+    if (item != '') {
+      if (item.length < 1 || item.length > 50) {
+        alert('1文字以上50文字以内で入力してください');
+      } else {
+        setItemList([...itemList, item]);
+        setItem('');
+      }
     }
   }
 
   const deleteItem = (index) => {
-    var newList = itemList.slice(0,itemList.length);
-    newList.splice(index,1);
+    var newList = itemList.slice(0, itemList.length);
+    newList.splice(index, 1);
     setItemList(newList);
   }
 
-  const postBucketList = async () =>{
-    setItemList([]);
-    setTitle('');
-    const bucketListJson = {
-      title : title,
-      items : itemList
-    }
-    const result = await axios.post('http://127.0.0.1:3000/api/createlist',bucketListJson)
-      .catch(err => {
-        alert(err);
-      });
-    if(result.status != 200){
-      console.log("error!");
-    }else{
-      alert('バケツリストを作成しました');
+  const postBucketList = async () => {
+    if (title.length < 1 || title.length > 50) {
+      alert('タイトルは1文字以上50文字以内で入力してください');
+    } else {
+      setItemList([]);
+      setTitle('');
+      const bucketListJson = {
+        title: title,
+        items: itemList
+      }
+      const result = await axios.post('http://127.0.0.1:3000/api/createlist', bucketListJson)
+        .catch(err => {
+          alert(err);
+        });
+      if (result.status != 200) {
+        console.log("error!");
+      } else {
+        alert('バケツリストを作成しました');
+      }
     }
   }
 
@@ -47,16 +55,16 @@ export const CreateList = () => {
         <table>
           <tr>
             <th className="px-3"><label>タイトル</label></th>
-            <th><input className="input_textbox" type="text" onChange={() => setTitle(event.target.value)} value={title}/></th>
+            <th><input className="input_textbox" type="text" onChange={() => setTitle(event.target.value)} value={title} /></th>
           </tr>
           <tr>
             <th><label className="px-3">項目</label></th>
             <th><input className="input_textbox" type="text" onChange={() => setItem(event.target.value)} value={item} /></th>
-            <th><input className="btn btn-primary" type="button" value="+" onClick={() => addItem() } /></th>
+            <th><input className="btn btn-primary" type="button" value="+" onClick={() => addItem()} /></th>
           </tr>
         </table>
       </form>
-      <ul className="list-group">{itemList.map((item,index)=><li className="list-group-item" key={index}>{item}<input className="btn btn-danger ml-3" type="button" value="☓" onClick={()=> deleteItem(index)}/></li>)}</ul>
+      <ul className="list-group">{itemList.map((item, index) => <li className="list-group-item" key={index}>{item}<input className="btn btn-danger ml-3" type="button" value="☓" onClick={() => deleteItem(index)} /></li>)}</ul>
       <input className="btn btn-success" type="button" value="作成" onClick={() => postBucketList()} />
     </div>
   );
